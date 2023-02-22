e5f3e855e90114c3d48e1d60949d9d6e52c27ea5
Vova <bungavolodya@gmail.com>
2023-02-22 12:34:26 +0200

    add lab2

---
+++ Volodya_Bunga/lab2/lab_2.py
@@ -0,0 +1,12 @@
+
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+
+if mystring == "hello":
+    print("String: " , mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: " , myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: " , myint)
---
+++ Volodya_Bunga/lab2/lab_21.py
@@ -0,0 +1,12 @@
+numbers = [1,2,3,4,5]
+strings = ["s","q","1"]
+names = ["John", "Eric", "Jessica"]
+
+
+second_name = names[1]
+
+
+# this code should write out the filled arrays and the second name in the names list (Eric).
+print(numbers)
+print(strings)
+print("The second name on the names list is " , second_name)
