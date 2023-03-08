117a0af17c6a2236df9b4c36a55c669d3a5c5e3b
Taras Vasylychyn <103831459+pettrichor@users.noreply.github.com>
2023-03-08 10:03:20 +0200

    change report

--- Taras_Vasylychyn/lab-5/lab-5.py
+++ Taras_Vasylychyn/lab-5/lab-5.py
@@ -1,26 +1,14 @@
-# define the Vehicle class
-class Vehicle:
-    name = ""
-    kind = ""
-    color = ""
-    value = 100.00
-    def description(self):
-        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
-        return desc_str
+class MyClass:
+    def __init__(self,a,b):
+        self.a = a
+        self.b = b
 
-# your code goes here
-car1 = Vehicle()
-car1.name = "Fer"
-car1.color = "red"
-car1.kind = "convertible"
-car1.value = 60000.00
 
-car2 = Vehicle()
-car2.name = "Jump"
-car2.color = "blue"
-car2.kind = "van"
-car2.value = 10000.00
+    def add(self):
+        sum = self.a + self.b
+        print(sum)
 
-# test code
-print(car1.description())
-print(car2.description())
+ob = MyClass(1,2)
+print(ob)
+
+ob.add()
